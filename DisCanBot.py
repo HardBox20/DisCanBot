41f8987e15c395148a57ab0805153a4fd675c6e0
HardBox20 <b.volkov.02062004@gmail.com>
2026-05-04 23:02:52 +0300

    Add files via upload

--- DisCanBot.py
+++ DisCanBot.py
@@ -1,10 +1,8 @@
 import discord
 from discord.ext import tasks
 import aiohttp
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 TOKEN = os.getenv("TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
